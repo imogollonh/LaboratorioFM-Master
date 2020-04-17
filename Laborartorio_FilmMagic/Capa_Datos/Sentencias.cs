54c57685cea17966801c7b50ad91f1634da0e8f8
imogollonh <imogollonh@miumg.edu.gt>
2020-04-17 17:20:46 -0600

    MDI General

---
+++ Laborartorio_FilmMagic/Capa_Datos/Sentencias.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Capa_Datos
+{
+    public class Sentencias
+    {
+        Conexion cn = new Conexion();
+        OdbcCommand comm;
+
+        //--------------------------------------------------------------------OBTENER COD SIGUIENTE--------------------------------------------------------------------//
+        public string obtenerfinal(string tabla, string campo)
+        {
+            String camporesultante = "";
+            try
+            {
+                string sql = "SELECT MAX(" + campo + "+1) FROM " + tabla + ";"; //SELECT MAX(idFuncion) FROM `funciones`     
+                OdbcCommand command = new OdbcCommand(sql, cn.conexionbd());
+                OdbcDataReader reader = command.ExecuteReader();
+                reader.Read();
+                camporesultante = reader.GetValue(0).ToString();
+
+                if (String.IsNullOrEmpty(camporesultante))
+                    camporesultante = "1";
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(camporesultante);
+            }
+            return camporesultante;
+        }
+
+    }
+}
---
+++ Laborartorio_FilmMagic/Capa_Logica/Logica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+
+namespace Capa_Logica
+{
+   public class Logica
+    {
+        Sentencias sn = new Sentencias();
+        /*OBTENER COD SIGUIENTE*/
+        public string siguiente(string tabla, string campo)
+        {
+            string llave = sn.obtenerfinal(tabla, campo);
+            return llave;
+        }
+
+    }
+}
